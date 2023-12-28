b9003fe363312c4c9ee2ed8b74d905b864dec135
Nilesh <ece.20becd76@silicon.ac.in>
2023-12-29 03:17:24 +0530

    [setup-button-theme]

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Switch, Text, View } from 'react-native';
+import { useColorScheme } from 'nativewind';
+
 
 export default function App() {
+  const {colorScheme,toggleColorScheme} = useColorScheme();
+  console.log(colorScheme);
   return (
-    <View style={styles.container}>
+    <View className="flex-1 justify-center items-center dark:bg-neutral-900">
+      <View className="flex-row justify-center items-center space-x-2">
+        <Text className="text-2xl font-bold dark:text-white8">Toggle Theme</Text>
+        <Switch value={colorScheme=='dark'} onChange={toggleColorScheme} />
+      </View> 
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
